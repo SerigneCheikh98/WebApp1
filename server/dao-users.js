36f567da4190b9d0bf7a830b9023ab6a7ab16ae5
SerigneCheikh98 <fallserignecheikh.98@gmail.com>
2023-06-14 23:50:52 +0200

    passport&session, login, logout, verifyAuth

--- server/dao-users.js
+++ server/dao-users.js
@@ -1,3 +1,52 @@
 'use strict';
 
 const db = require('./db');
+const crypto = require('crypto');
+
+// This function is used at log-in time to verify username and password.
+exports.getUser = (username, password) => {
+  return new Promise((resolve, reject) => {
+    const sql = 'SELECT * FROM Users WHERE username=?';
+    db.get(sql, [username], (err, row) => {
+      if (err) {
+        reject(err);
+      } else if (row === undefined) {
+        resolve(false);
+      }
+      else {
+        const user = { id: row.id, username: row.username, role: row.role };
+
+        // Check the password
+        crypto.scrypt(password, row.salt, 32, function (err, hashedPassword) {
+          if (err) {
+            reject(err);
+          }
+          if (!crypto.timingSafeEqual(Buffer.from(row.password, 'hex'), hashedPassword))
+            resolve(false);
+          else
+            resolve(user);
+        });
+      }
+    });
+  });
+};
+
+
+// This function returns user's information given its id.
+exports.getUserById = (id) => {
+  return new Promise((resolve, reject) => {
+    const sql = 'SELECT * FROM Users WHERE id=?';
+    db.get(sql, [id], (err, row) => {
+      if (err)
+        reject(err);
+      else if (row === undefined)
+        resolve({ error: 'User not found.' });
+      else {
+        // By default, the local strategy looks for "username": 
+        // for simplicity, instead of using "email", we create an object with that property.
+        const user = { id: row.id, username: row.username, role: row.role }
+        resolve(user);
+      }
+    });
+  });
+};
--- server/index.js
+++ server/index.js
@@ -2,29 +2,93 @@
 
 const PORT = 3000;
 
+const express = require('express');
+
 // Get modules accessing the DB
 const usersDao = require('./dao-users');
 const pagesDao = require('./dao-pages'); 
 
-const express = require('express');
+// Middlewares
 const morgan = require('morgan');
 const cors = require('cors');
+const session = require('express-session');
+
+// Passport
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
 
 const app = express();
+// Register middlewares
 app.use(morgan('combined'));
 app.use(express.json());
 app.use(cors());
 
-/**
- * Authentication APIs
- */
-const login = async (req, res) => {
+app.use(session({
+    secret: "Deboli o forti, intelligenti o semplici, noi simo tutti fratelli...Good luck with this secret!",
+    resave: false,
+    saveUninitialized: false,
+}));
+
+// Set up authentication strategy to search in the DB a user with a matching password.
+passport.use(new LocalStrategy(async function verify(username, password, callback) {
+  const user = await usersDao.getUser(username, password)
+  if(!user){
+    return callback(null, false, 'Invalid username or password');  
+  }
+  return callback(null, user);
+}));
+
+app.use(passport.authenticate('session'));
+
+// Serializing user in the session
+passport.serializeUser((user, callback) => {
+    callback(null, { id: user.id, username: user.username });
+});
+// Deserializing user from the session
+passport.deserializeUser((user, callback) => {
+    callback(null, user);
+});
+
+// Login status checker middleware
+const isLoggedIn = (req, res, next) => {
+    if(req.isAuthenticated()) {
+      return next();
+    }
+    return res.status(401).json({ error: 'Not authorized' });
 }
 
-const logout = async (req, res) => {
+/******************** Authentication APIs ********************/
+
+const login = async (req, res, next) => {
+    passport.authenticate('local', (err, user, info) => {
+        if (err)
+            return next(err);
+        if (!user) {
+            return res.status(401).json({ error: info });
+        }
+        // success, perform the login and extablish session
+        req.login(user, (err) => {
+            if (err)
+                return next(err);
+
+            return res.json(req.user);
+        });
+    })(req, res, next);
 }
 
-const verifyAuth = async (req, res) => {
+const logout = (req, res) => {
+    req.logout(() => {
+        res.status(200).json({message: "User logged out"});
+    });
+}
+
+const verifyAuth = (req, res) => {
+    if(req.isAuthenticated()){
+        res.status(200).json(req.user);
+    }
+    else{
+        res.status(401).json({error: 'Not authenticated'});
+    }
 }
 
 app.post('/api/login', login);
@@ -32,9 +96,7 @@ app.get('/api/verifyAuth', verifyAuth);
 app.delete('/api/logout', logout);
 
 
-/**
- * Front-office and Back-office APIs
- */
+/******************** Front-office and Back-office APIs ********************/
 
 const getAllPages = async (req, res) => {
     res.status(200).json("Hello World!")
